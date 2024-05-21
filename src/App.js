073b438d9d7e5a9716358e11c9a82a3f8e6df8b6
Devansh27112001 <dkansara2711@gmail.com>
2024-05-21 17:03:50 -0400

    Initial data fetching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+const initialState = {
+  questions: [],
+  // 'loading','error','ready','active',''finished'
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Unknown Action");
+  }
+}
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
