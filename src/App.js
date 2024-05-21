d42cb1bff77cd9c2be6106a514ce75c9a41c6d5e
Devansh27112001 <dkansara2711@gmail.com>
2024-05-21 16:34:48 -0400

    Added a main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 export default function App() {
   return (
     <div className="app">
       <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
